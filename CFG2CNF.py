c37825e8f53968325f12a761986346431d1964ac
MuslimMujahid <muslimmujahid1712@gmail.com>
2019-11-25 23:30:57 +0700

    cfg to cnf udah bisa

--- CFG2CNF.py
+++ CFG2CNF.py
@@ -20,27 +20,40 @@ def cleanProduction(expression):
             result.append((leftside, term.split(" ")))
     return result
 
+def isExistNullProduction(productions, variables):
+    for rule in productions:
+        left, right = rule
+        if "e" in right:
+            return True
+    return False
+
 def START(productions, variables):
     variables.append("S0")
     return [("S0", [variables[0]])] + productions
 
 def REMOVE_NULL_PRODUCTIONS(productions, variables):
-    NullVariables = []
-    for rule in productions:
-        left, right = rule
-        if isNullProduct(rule):
-            NullVariables.append(left)
-            productions.remove(rule)
 
-    for NullVariable in NullVariables:
-        NullVariablesRemoved = []
-        for rule in productions:
+    while ( isExistNullProduction(productions, variables) ):
+        NullVariables = []
+        CopyProductions = productions.copy()
+        for rule in CopyProductions:
             left, right = rule
-            if isExistNullVar(rule, NullVariable):
-                NullVariablesRemoved += replaceNullVar(rule, NullVariable)
-        for rule in NullVariablesRemoved:
-            if rule not in productions:      
-                productions.append(rule)
+            if isNullProduct(rule):
+                NullVariables.append(left)
+                productions.remove(rule)
+                
+        for NullVariable in NullVariables:
+            NullVariablesRemoved = []
+            for rule in productions:
+                left, right = rule
+                if isExistNullVar(rule, NullVariable):
+                    NullVariablesRemoved += replaceNullVar(rule, NullVariable)
+            for rule in NullVariablesRemoved:
+                if rule not in productions:
+                    left, right = rule 
+                    if len(right) == 0:
+                        right.append("e")
+                    productions.append(rule)
 
     return productions
 
@@ -83,7 +96,7 @@ def replaceNullVar(rule, NullVar):
 
 def isUnitProduct(rule, variables):
     left, right = rule
-    return len(right) == 1 and right[0] in variables and not (left == "S0")
+    return len(right) == 1 and right[0] in variables
 
 def isExistUnitProduct(productions, variables):
     exist = False
@@ -93,36 +106,45 @@ def isExistUnitProduct(productions, variables):
             break
     return exist
 
-def replaceUnitProduct(productions, rule):
+def replaceUnitProduct(productions, variables, rule):
+    result = []
     left, right = rule
     for ruleS in productions:
         leftS, rightS = ruleS
-        if leftS == right[0]:
-            return [(left, rightS)]
+        if leftS == right[0] and not isUnitProduct(ruleS, variables):
+            result.append((left, rightS))
+
+    return result
 
 def isRuleUnreachable(productions, variables, rule):
     left, right = rule
     while True:
+        unreachable = True
         for ruleS in productions:
             leftS, rightS = ruleS
-            if left in rightS:
+            if left in rightS and not(left == leftS):
                 left = leftS
+                unreachable = False
                 break
-        break
+        if unreachable:
+            break
 
     return left is not "S0"
 
 
 def REMOVE_UNIT_PRODUCTIONS(productions, variables):
 
-    while isExistUnitProduct(productions, variables):
-        unitProductions = []
-        for rule in productions:            
-            if isUnitProduct(rule, variables):
-                unitProductions.append(rule)
+    CopyProductions = productions.copy()
+    for rule in CopyProductions:            
+        if isUnitProduct(rule, variables) and rule in productions:
+            if rule in productions:
                 productions.remove(rule)
-        for unitProduction in unitProductions:
-            productions += replaceUnitProduct(productions + unitProductions, unitProduction)
+            newRules = replaceUnitProduct(productions, variables, rule)
+            for newRule in newRules:
+                if newRule not in productions:
+                    productions.append(newRule)
+            
+                
 
     CopyProductions = productions.copy()
     for rule in CopyProductions:
@@ -146,7 +168,9 @@ def replaceMoreThan2Var(variables, rule, Vars):
     result = []
     left, right = rule
     while isVariablesMoreThan2(variables, rule):
-        result.append((Vars.pop(0),[right.pop(-1)]))
+        leftS = Vars.pop(0)
+        result.append((leftS, [right.pop(-2)]+[right.pop(-1)]))
+        right.append(leftS)
 
     # print(result)
     return Vars, result+[(left, right)]
@@ -171,25 +195,31 @@ def getNotUsedVariables(usedVariables, Vars):
         Vars.remove(V)
     return Vars
 
-def replaceTermProduction(variables, rule, Vars):
+def replaceTermProduction(productions, variables, rule, Vars):
+
     result = []
     left, right = rule
-    leftS = Vars.pop(0)
-    rightS = [right[0]]
-    result += [(leftS, rightS)]
-    right[0] = leftS
-    result += [(left, right)]
+    newLeft = Vars.pop(0)
+    result.append((newLeft,[right[0]]))
 
-    return Vars, result
+    CopyProductions = productions.copy()
+    for ruleS in CopyProductions:
+        leftS, rightS = ruleS
+        if len(rightS) == 2 and rightS[0] == right[0]:
+            newRule = (leftS, [newLeft,rightS[0]])
+            if newRule not in productions:
+                result.append((leftS, [newLeft,rightS[0]]))
+                productions.remove(ruleS)
+
+    productions += result
+    return Vars, productions
 
 def REMOVE_TERM_PRODUCTION(productions, variables, Vars):
     CopyProductions = productions.copy()
-    for rule in CopyProductions:
+    for rule in productions:
         left, right = rule
         if isTermProduction(variables, rule):
-            productions.remove(rule)
-            Vars, newRule = replaceTermProduction(variables, rule, Vars)
-            productions += newRule
+            Vars, productions = replaceTermProduction(productions, variables, rule, Vars)
     return Vars, productions
 
 K, V, Productions = loadModel("model4.txt")
@@ -197,9 +227,9 @@ varContainer = getNotUsedVariables(V, varContainer)
 
 Productions = START(Productions, V)
 Productions = REMOVE_NULL_PRODUCTIONS(Productions, V)
-# Productions = REMOVE_UNIT_PRODUCTIONS(Productions, V)
-# varContainer, Productions = REMOVE_MORE_THAN_2_VARIABLES_PRODUCTION(Productions, V, varContainer)
-# varContainer, Productions = REMOVE_TERM_PRODUCTION(Productions, V, varContainer) 
+Productions = REMOVE_UNIT_PRODUCTIONS(Productions, V)
+varContainer, Productions = REMOVE_MORE_THAN_2_VARIABLES_PRODUCTION(Productions, V, varContainer)
+varContainer, Productions = REMOVE_TERM_PRODUCTION(Productions, V, varContainer) 
 
 for rule in Productions:
     print(rule)
