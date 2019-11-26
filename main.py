3a58bbecfdc3125fcc7bd29355d65b8e47455f41
MuslimMujahid <muslimmujahid1712@gmail.com>
2019-11-27 01:14:17 +0700

    cobain

--- main.py
+++ main.py
@@ -2,21 +2,23 @@ import CFG2CNF
 import CYK 
 def readSyntax(Terminals, languages):
     syntax = languages
-    syntax.replace("\t", " ") 
-    syntax.replace("\n", " NEWLINE ")
+    syntax = syntax.replace("\t", "") 
+    syntax = syntax.replace("\n", " ENDL ")
     syntax = syntax.split(" ")
     for x in range (syntax.count('')):
         syntax.remove('')
     for i in range(len(syntax)):
         if syntax[i] not in Terminals and len(syntax[i]) > 0:
-            syntax[i] = "NAME"
+            syntax[i] = "UNKNOWN"
     return syntax
 
 
-Terminals, CNF = CFG2CNF.CFG2CNF("model.txt")
+Terminals, CNF = CFG2CNF.CFG2CNF("model2.txt")
 languages = open("syntax.txt").read()
 languages = readSyntax(Terminals, languages)
-print(languages)
 # for x in CNF:
 #     print(x)
+for x in languages:
+    print(x)
+# print(languages)
 CYK.CYK(CNF, 'S0', languages)
