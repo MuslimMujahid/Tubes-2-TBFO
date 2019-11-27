659c73f929dd90118b355c8c1fa429ce9c09b32f
MuslimMujahid <muslimmujahid1712@gmail.com>
2019-11-27 10:14:07 +0700

    tc4 tc5

--- main.py
+++ main.py
@@ -1,40 +1,54 @@
 import CFG2CNF
 import CYK 
 
-tokens = [':', '.', '+', '-', '*', '/', '<', '>', '<=', '>=', '==', '(', ')', '[', ']', ',', "' '", "'"]
-languages = open("syntax.txt").read() + '\nqwertyuiop'
+tokens = [':', '.', '+', '-', '*', '/', '<', '>', '<=', '>=', '==', '(', ')', '[', ']', ',', "' '", "'", "\""]
+languages = open("syntax.txt").read()
+# print(languages)
 
 def readSyntax(Terminals, languages):
-    syntax = languages
+    syntax = languages   
+    if syntax[-1] == ")":
+        syntax = syntax[:-1] + " )"
+    syntax = syntax.replace("  ", " ")
+    # print(syntax)
     syntax = syntax.replace("\t", "") 
     syntax = syntax.replace("\n", " ENDL ")
     syntax = syntax.replace("' '", " UNKNOWN ")
+    syntax = syntax.replace("\" \"", " UNKNOWN ")
     syntax = syntax.split(" ")
     for x in range (syntax.count('')):
         syntax.remove('')
-    
+    # print(syntax)
     for i in range(len(syntax)):
         if syntax[i] not in Terminals and len(syntax[i]) > 0:
             syntax[i] = "UNKNOWN"
+    # print(syntax)
     return syntax
 
 Terminals, CNF = CFG2CNF.CFG2CNF("model2.txt")
-languages = open("tc2.txt").read()
+languages = open("tc5.txt").read()
 
+# print(languages)
 for token in tokens:
     read = len(token)
-    for i in range(len(languages)-read):
+    for i in range(len(languages)-read+1):
         CC = languages[i:i+read]
         if CC == token:
+            # print(CC)
             if not (languages[i-1] == " "):
+                # print("[i:]", languages[i:])
                 languages = languages[:i] + " " + languages[i:]
-            elif not (languages[i+read+1] == " "):
-                languages = languages[:i+read] + " " + languages[i+read:]
-                
-languages = languages[:len(languages)-11]
-
+            elif not ( i == len(languages)-2):
+                if not (languages[i+read+1] == " "):
+                    languages = languages[:i+read] + " " + languages[i+read:]
+# print(languages)
 languages = readSyntax(Terminals, languages)
+# print(languages)
+# remove endl in end of syntax
+if languages[-1] == "ENDL":
+    languages = languages[:-1]
 
+# print(languages)
 if ( CYK.CYK(CNF, 'S0', languages) ):
     print("Compile success .... ")
 else :
